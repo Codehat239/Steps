7fd9c0436e669a42583eff2a1df500881901d6a5
Richmond Blankson <mensahrichmondb@gmail.com>
2025-01-02 09:54:51 +0000

    add close button to dynamically rerender the view

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import React, { useState } from "react";
 
 const messages = ["Learn React *", "Apply for jobs", "Invest your new income."];
-const jobs = ["Food vendor", "Musician", "Digital Artist"];
 
 function App() {
   const btnStyle = { backgroundColor: "#7950f2", color: "#fff" };
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = () => {
     if (step > 1) {
-      setStep((prevValue) => prevValue - 1);
+      setStep(step - 1);
     }
   };
 
   const handleNext = () => {
     if (step < 3) {
-      setStep((prevValue) => prevValue + 1);
+      setStep(step + 1);
     }
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button style={btnStyle} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={btnStyle} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button style={btnStyle} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={btnStyle} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
